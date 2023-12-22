f6ac4eb94c3589c7c38b76fc41b2615d79e23d1a
Vojtech Benes <vojtech.benes@carvago.com>
2023-12-22 15:11:20 +0100

    update readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {App} from './App';
-import reportWebVitals from './reportWebVitals';
 import {GlobalStyle} from "./GlobalStyle";
 import {ThemeProvider} from "styled-components";
 import {theme} from "./theme";
@@ -18,5 +16,3 @@ root.render(
         </ThemeProvider>
     </React.StrictMode>
 );
-
-reportWebVitals();
